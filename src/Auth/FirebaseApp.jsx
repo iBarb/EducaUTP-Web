795022431180b0c64469bc622c85a56f15727283
Bruno-Desktop <brunobarbaran@hotmail.com>
2024-07-20 20:27:56 -0500

    set env variables

--- src/Auth/FirebaseApp.jsx
+++ src/Auth/FirebaseApp.jsx
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB_RlB_FCQQg1-n59UNXzB9duiHZfLGLdA",
-    authDomain: "educautp.firebaseapp.com",
-    projectId: "educautp",
-    storageBucket: "educautp.appspot.com",
-    messagingSenderId: "804707948008",
-    appId: "1:804707948008:web:987cae001948e0a880405d"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
